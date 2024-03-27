7edbc71075b8281f9cc060191d4ef37ef20bebb4
dongchengjie <2424014450@qq.com>
2024-03-28 07:21:58 +0800

    chore: rename

--- metaST/Src/Core/Meta/Config/MetaPostProperty.cs
+++ metaST/Src/Core/Meta/Config/MetaPostProperty.cs
@@ -14,7 +14,7 @@ public partial class MetaPostProperty
         MatchCollection matchCollection = regex.Matches(config);
         List<GroupCollection> groupCollections = matchCollection.Select(match => match.Groups).DistinctBy(groups => groups[0].Value).ToList();
         // 生成替换映射
-        Dictionary<string, string> replacements = groupCollections.ToDictionary(groups => groups[0].Value, groups => ProrpertyValue(groups[1].Value));
+        Dictionary<string, string> replacements = groupCollections.ToDictionary(groups => groups[0].Value, groups => ResolveProrpertyValue(groups[1].Value));
         // 替换掉占位符
         foreach (KeyValuePair<string, string> entry in replacements)
         {
@@ -23,16 +23,16 @@ public partial class MetaPostProperty
         return config;
     }
 
-    private static string ProrpertyValue(string prorperty)
+    private static string ResolveProrpertyValue(string prorperty)
     {
         // 处理命令行参数
-        if (prorperty.StartsWith("options")) return OptionValue(prorperty);
+        if (prorperty.StartsWith("options")) return ResolveOptionValue(prorperty);
         // 处理图标资源
-        if (prorperty.StartsWith("icons")) return IconBase64(prorperty);
+        if (prorperty.StartsWith("icons")) return ResolveIcon(prorperty);
         throw new InvalidDataException($"处理配置属性值错误，未知的属性: {prorperty}");
     }
 
-    private static string OptionValue(string prorperty)
+    private static string ResolveOptionValue(string prorperty)
     {
         CommandLineOptions options = Context.Options;
         string json = JsonConvert.SerializeObject(options);
@@ -45,7 +45,7 @@ public partial class MetaPostProperty
         return string.Empty;
     }
 
-    private static string IconBase64(string prorperty)
+    private static string ResolveIcon(string prorperty)
     {
         byte[] bytes = Resources.ReadAsBytes(prorperty);
         bytes = bytes.Length > 0 ? bytes : Resources.ReadAsBytes(prorperty = "icons.unknown.svg");
