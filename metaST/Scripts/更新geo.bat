72aa7c9cebb57f8aae00ccf733b4872fc8a5a017
dongchengjie <37543964+dongchengjie@users.noreply.github.com>
2025-01-01 05:58:36 +0800

    chore: update mihomo

--- metaST/Scripts/更新geo.bat
+++ metaST/Scripts/更新geo.bat
@@ -6,10 +6,13 @@ echo 下载geosite.dat
 curl -L -s -o geosite.dat  https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
 echo 下载country.mmdb
 curl -L -s -o country.mmdb https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb
+echo 下载ASN.mmdb
+curl -L -s -o ASN.mmdb https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb
 
 echo 移动文件...
 move /Y geoip.dat    ..\Resources\meta\geoip.dat >nul
 move /Y geosite.dat  ..\Resources\meta\geosite.dat >nul
 move /Y country.mmdb ..\Resources\meta\country.mmdb >nul
+move /Y ASN.mmdb ..\Resources\meta\ASN.mmdb >nul
 
 pause
